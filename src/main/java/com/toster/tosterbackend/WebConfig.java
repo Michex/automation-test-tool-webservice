1dbbf7d7058a158e5bf1356523bccb528ca92eaf
msasin <michal.sasin@deviniti.com>
2020-02-27 13:42:37 +0100

    add config for swagger

--- src/main/java/com/toster/tosterbackend/WebConfig.java
+++ src/main/java/com/toster/tosterbackend/WebConfig.java
@@ -3,6 +3,7 @@ package com.toster.tosterbackend;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
 
